7aed64ea7f58416ef341732835ba63139eb95aee
Wang Yiyao <viana37@sina.com>
2016-01-14 21:21:14 +0800

    javascript and dom

---
+++ motation/prepareSlideshow.js
@@ -0,0 +1,26 @@
+function prepareSlideshoe(){
+	if(!document.getElementsByName) return false;
+	if(!document.getElementById) return false;
+	if(!document.getElementById("linklist")) return false;
+	if(!document.getElementById("preview")) return false;
+
+	var preview = document.getElementById("preview");
+	preview.style.position = "absolute";
+	preview.style.left = "0px";
+	preview.style.top = "0px";
+
+	var list = document.getElementById("linklist");
+	var lists = document.getElementsByTagName("a");
+	lists[0].onmouseover = function(){
+		moveElement("preview",-100, 0, 10);
+	}
+	lists[1].onmouseover = function(){
+		moveElement("preview", -200, 0, 10);
+	}
+	lists[2].onmouseover = function(){
+		moveElement("preview", -300, 0, 10);
+	}
+
+}
+
+addLoadEvent(prepareSlideshoe);
